a3fd84127a5011fe7103634635414e10ac64cbc7
Bamzy123 <stephenomotos@gmail.com>
2026-05-11 22:29:46 +0100

    latest update

--- arrays.js
+++ arrays.js
@@ -63,4 +63,60 @@ console.log(prices[2]); // output: [{ name: "Laptop", price: 1000, inStock: true
 
 // filter products that are in stock
 const electronics = products.filter(product => product.categories === "Electronics"); // filter products by category
-console.log(electronics); // output: [{ name: "Laptop", price: 1000, inStock: true, categories: "Electronics" }, { name: "Phone", price: 500, inStock: true, categories: "Electronics" }]
+console.log(electronics[0], electronics[1]); // output: [{ name: "Laptop", price: 1000, inStock: true, categories: "Electronics" }, { name: "Phone", price: 500, inStock: true, categories: "Electronics" }]
+
+//====================OBJECTS===================//
+
+ const developer = {
+    name: "Ayo",
+    age: 25,
+    location: "Lagos",
+    skills: ["HTML", "CSS", "JavaScript"],
+    isAvailable: true,
+    rates: {
+        hourly: 5000,
+        daily: 40000,
+        weekly: 200000
+    },
+    introduce() {
+        return `Hi, I'm ${this.name}, a developer with skills in ${this.skills.join(", ")}. I am currently ${this.isAvailable ? "available"
+             : "unavailable"} for work. My rates are ₦${this.rates.hourly}/hour,
+             ₦${this.rates.daily}/day, and ₦${this.rates.weekly}/week.`;
+    },
+
+    getLoation() {
+        return `I am ${this.name} based in ${this.location}.`;
+    }
+ };
+
+ console.log(developer.introduce()); // output: Hi, I'm Ayo, a developer with skills in HTML, CSS, JavaScript. I am currently available for work. My rates are ₦5000/hour, ₦40000/day, and ₦200000/week.
+
+ console.log(developer.getLoation()); // output: I am based in Lagos.
+
+ developer.portfolio = "https://ayodev.com"; // adding a new property to the object
+ console.log(developer.portfolio); // output: https://ayodev.com
+ console.log(developer); // output: { name: "Ayo", age: 25, location: "Lagos", skills: ["HTML", "CSS", "JavaScript"], isAvailable: true, rates: { hourly: 5000, daily: 40000, weekly: 200000 }, introduce: [Function: introduce], getLoation: [Function: getLoation], portfolio: "https://ayodev.com" }
+
+developer.skills.push("React"); // adding a new skill to the skills array
+console.log(developer.skills); // output: ["HTML", "CSS", "JavaScript", "React"]
+
+delete developer.skills[3]; // removing a skill from the skills array
+console.log(developer.skills); // output: ["HTML", "CSS", "JavaScript"]
+
+// DESTRUCTURING AN OBJECT
+const student = {
+    name: "Iyanuoluwa",
+    age: 22,
+    city: "Ife",
+    course: "Web Development",
+    hasPaid: true,
+    skills: ["HTML", "CSS", "JavaScript"]
+};
+
+const { name, age, city, course, hasPaid, skills } = student; // destructuring the student object
+console.log(name, age, city, course, hasPaid, skills);
+// console.log(age);
+// console.log(city);
+// console.log(course);
+// console.log(hasPaid);
+// console.log(skills);
